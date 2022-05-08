547aea5ec2b30543d075c70923921ebbe88061f9
Cyb3r3xel <97148124+Cyb3r3xel@users.noreply.github.com>
2022-05-08 21:31:59 +0400

    Update Token Generator.py

--- Token Generator.py
+++ Token Generator.py
@@ -61,25 +61,49 @@ if opcion=='1':
             pass
     pass
 if opcion=='2':
-    os.system("cls")
-    print("\n" + banner + "\n")
-    with open('Generated.txt', 'r') as f:
-        for line in f:
-            time.sleep(0)
-            token = line.rstrip("\n")
-            headers = {
-                'Authorization': f'{token}'  
-            }
-            src = requests.get('https://discordapp.com/api/v6/auth/login', headers=headers)
- 
-            try:
-                if src.status_code == 200:
-                    print(f'{Fore.BLUE}InValid token! >{Fore.RESET} ' + token)
+from requests import get, post
+from random import randint
+
+def variant1(token):
+    response = get('https://discord.com/api/v6/auth/login', headers={"Authorization": token})#Bad variant for mass token check due to the rate limit.
+    return True if response.status_code == 200 else False
+
+def variant2(token):
+    response = post(f'https://discord.com/api/v6/invite/{randint(1,9999999)}', headers={'Authorization': token})
+    if "You need to verify your account in order to perform this action." in str(response.content) or "401: Unauthorized" in str(response.content):
+        return False
+    else:
+        return True
+
+def variant2_Status(token):
+    response = post(f'https://discord.com/api/v6/invite/{randint(1,9999999)}', headers={'Authorization': token})
+    if response.status_code == 401:
+        return 'Invalid'
+    elif "You need to verify your account in order to perform this action." in str(response.content):
+        return 'Phone Lock'
+    else:
+        return 'Valid'
+
+if __name__ == "__main__":
+    try:
+        checked = []
+        with open('Generated.txt', 'r') as tokens:
+            for token in tokens.read().split('\n'):
+                if len(token) > 15 and token not in checked and variant2(token) == True:
+                    print(f'Token: {token} is Valid')
+                    checked.append(token)
                 else:
-                    print(f'{Fore.LIGHTGREEN_EX}Valid token >{Fore.RESET} ' + token)
-            except Exception:
-                print(f"{Fore.CYAN}Error, please contact with ! Cyber™#1996 {Fore.RESET}")
-pass
+                    print(f'Token: {token} is Invalid')
+        if len(checked) > 0:
+            save = input(f'{len(checked)} valid tokens\nSave to File (y/n)').lower()
+            if save == 'y':
+                name = randint(100000000, 9999999999)
+                with open(f'{name}.txt', 'w') as saveFile:
+                    saveFile.write('\n'.join(checked))
+                print(f'Tokens Save To {name}.txt File!')
+        input('Press Enter For Exit...')
+    except:
+        input('Can`t Open "Generated.txt" File!')
 if opcion=='3':
     os.system("cls")
     print(Fore.WHITE +Fore.BLUE +"                         ["+Fore.WHITE +Fore.BLUE +"+"+Fore.WHITE +Fore.BLUE +"]"+ Fore.WHITE +Fore.BLUE +"-------------------------------------------------------"+ Fore.WHITE +Fore.BLUE +"["+ Fore.WHITE +Fore.BLUE +"+"+ Fore.WHITE +Fore.BLUE +"]")
